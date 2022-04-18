538dbc66d216d0ab5bad5f8753f22cfd23d631e3
mcdavidxii <realmcdavid1@gmail.com>
2022-04-18 08:41:58 +0200

    movies fetch

--- src/Banner.js
+++ src/Banner.js
@@ -1,11 +1,44 @@
-import React from 'react'
+import axios from './axios';
+import React, { useEffect, useState } from 'react'
+import requests from './Requests'
 import'./Banner.css'
 
 function Banner() {
+
+    //   ---------------------------fetching the movies---------------------------------
+
+    const [movie,setMovie] = useState([]);
+
+    useEffect(() => {
+        async function fetchData(){
+            const request = await axios.get(requests.fetchNeflixOrginals);
+            setMovie(
+                request.data.results[
+                    Math.floor(Math.random()* request.data.results.length-1)
+                ]
+            );
+            return request;
+        }
+        fetchData();
+    },[]);
+
+    console.log(movie);
+
+    //------------------------------------------------------------------------------------
+
+
+    // ---------------- reduce the amount of characters in description if above 150-----------
+
+    function truncate(string, numberOfCharacters ){
+        return string?.length > numberOfCharacters ? string.substr(0,numberOfCharacters-1) + "..." :string;
+    }
+
+    //---------------------------------------------------------------------------------------
+
   return (
     <header className='banner' 
     style={{backgroundSize: "cover", 
-    backgroundImage:`url("https://i.imgur.com/g0jXdZC.jpg")`,
+    backgroundImage:`url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
     backgroundPosition:"center center"}
     }>
         <div className='banner_contents' >
@@ -14,7 +47,16 @@ function Banner() {
                 <button className='banner_button' >Play</button>
                 <button className='banner_button' >My List</button>
             </div>
-            <h1 className='banner_description' > Description testing </h1>
+            <h1 className='banner_description' > 
+            {truncate(`Description testing of the movie that was acted by James Blaker.
+            Description testing of the movie Description testing of the movie 
+            Description testing of the movie Description testing of the movie 
+            Description testing of the movie Description testing of the movie 
+            Description testing of the movie that was acted by James Blaker.
+            Description testing of the movie Description testing of the movie 
+            Description testing of the movie Description testing of the movie 
+            Description testing of the movie Description testing of the movie `,150)}
+            </h1>
         </div>
         <div className='banner--fadeBottom' />
     </header>
--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(  <App />);
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
