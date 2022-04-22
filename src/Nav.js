98bb9172b908c5878d67436475a07043e9673aa4
mcdavidxii <realmcdavid1@gmail.com>
2022-04-22 19:43:14 +0200

    error sorted

--- src/Nav.js
+++ src/Nav.js
@@ -22,12 +22,12 @@ function Nav() {
     );
 
 
-    const netflixLogo ='http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png';
+    const netflixLogo ='https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png';
     const netflixAvatar = 'https://i.pinimg.com/originals/1b/71/b8/1b71b85dd741ad27bffa5c834a7ed797.png';
     return (
         <div className={`nav ${show && "nav_black"}`}>
             <img
-                className='nav_0logo'
+                className='nav_logo'
                 src={netflixLogo}
                 alt='Netflix Logo'
             />
