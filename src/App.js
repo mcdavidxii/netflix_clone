043924aa4fa12ba80992be99b95909de5b36c176
mcdavidxii <realmcdavid1@gmail.com>
2022-04-22 22:22:04 +0200

    final frontend build

--- src/App.js
+++ src/App.js
@@ -11,8 +11,40 @@ function App() {
     <div className="App">
        {/* Nav here.. */}
        <Nav />
-
        <Banner />
+        <Row
+            title= 'NETFLIX ORIGINALS'
+            fetchUrl={requests.fetchNetflixOriginals}
+            isLargeRow
+        />
+        <Row
+            title='Top Rated'
+            fetchUrl={requests.fetchTopRated}
+        />
+        <Row
+            title='Action Movies'
+            fetchUrl={requests.fetchActionMovies}
+        />
+        <Row
+            title='Comedy Movies'
+            fetchUrl={requests.fetchComedyMovies}
+        />
+        <Row
+            title=''
+            fetchUrl={requests}
+        />
+        <Row
+            title='Horror Movies'
+            fetchUrl={requests.fetchHorrorMovies}
+        />
+        <Row
+            title='Documentaries'
+            fetchUrl={requests.fetchDocumentaries}
+        />
+        <Row
+            title='Romance Movies'
+            fetchUrl={requests.fetchRomanceMovies}
+        />
     </div>
   );
 }
