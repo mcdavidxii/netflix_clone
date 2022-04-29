87937f38716364a3f88e423a98a1d2b45b01f029
mcdavidxii <realmcdavid1@gmail.com>
2022-04-30 00:01:39 +0200

    firebase

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -1,5 +1,6 @@
 import react, {useRef } from "react";
 import { auth } from "../firebase";
+
 import "./SignUp.css";
 
 
@@ -11,10 +12,10 @@ function SignUp () {
     const register = (e) => {
         e.preventDefault();
 
-        auth.createUSerWithEmailAndPassword(
+        auth.createUserWithEmailAndPassword(
             emailRef.current.value,
             passwordRef.current.value
-        ).then( () => {
+        ).then( (authUser) => {
             console.log(authUser);
         }).catch( error => {
             alert(error.message);
@@ -24,15 +25,9 @@ function SignUp () {
     };
     const signIn = (e) => {
         e.preventDefault();
-     
-        auth.signInWithEmailAndPassword(
-            emailRef.current.value,
-            passwordRef.current.value
-        ).then( (authUser) => {
-            console.log(authUser);
-        }).catch( error => 
-            alert(error.message));
         
+     
+       
 
     }
 
